9ceab4706f35f2ed741f927068c160e49c228203
Kameni Alex <kamenialexnea@gmail.com>
2020-12-19 18:39:15 +0100

    fetch data in local or online

--- google_ensp/search_app/models.py
+++ google_ensp/search_app/models.py
@@ -4,12 +4,21 @@ import numpy as np
 import os
 import ml_models.tools.models as m
 import glob
+import requests
+
+
 
 # Create your models here.
 
 class EngineModel():
     def __init__(self,):
-        pass
+        self.headers = { 
+            "apikey": "60e07650-2df1-11eb-96e2-bf23fa38d811"
+        }
+        self.params = None
+        self.response = None
+        self.base_url = 'https://app.zenserp.com/api/v2/'
+        self.columns = ["url", "title", "description", "destination"]
 
     def clean_text(self, question:str, answers:pd.DataFrame, column="snippet"):
         """
@@ -25,8 +34,16 @@ class EngineModel():
         data = pd.DataFrame(np.append([question], answers[column].values), columns=["text"])
         data = m.clean_dataframe(data)
         return data
+    
+    def _fetch_exemples(self, path="./ml_models/exemple/queries.txt", save_to="./ml_models/exemple/", count=70):
+        questions = None
+        with open(path, 'rb') as file:
+            questions = file.readlines()
+        for i, question in enumerate(questions):
+            _, data = self._fetch_data(question)
+            data.to_csv(save_to+"query{}.csv".format(i), index=False, header=True)
 
-    def _fetch_data(self, query):
+    def _fetch_data(self, query=None, count=70):
         """
             Chercher du contenu éducatif sur internet
             Params:
@@ -34,11 +51,20 @@ class EngineModel():
             Return:
                 question, answers
                     - question: str
-                    - answers: pd.DataFrame: [link, title, snippet]
+                    - answers: pd.DataFrame: ["url", "title", "description", "destination"]
         """
-        pass
+        if query is None:
+            return self._fetch_data_offline()
+        self.params = (
+            ("q", query),
+            ("num", str(count)),
+        )
+        self.query = query
+        self.response = requests.get(self.base_url+"search", headers=self.headers, params=self.params)
+        self.data = pd.DataFrame(self.response.json()["organic"])
+        return query, self.data[self.columns].copy()
 
-    def _fetch_data_offline(self, path='./ml_models/exemple/*.csv'):
+    def _fetch_data_offline(self, path='./ml_models/exemple/query*.csv', queries_path='./ml_models/exemple/queries.txt'):
         """
             Appliquer les différents modèles sur des données en local
             Params:
@@ -52,9 +78,13 @@ class EngineModel():
         """
         np.random.seed()
         exemples = glob.glob(path)
-        choice = np.random.choice(exemples)
-        data = pd.read_csv(choice)
-        return data
+        if len(exemples) == 0:
+            raise Exception("No queries examples found in {}".format(path))
+        path_queries_index = np.random.choice(range(len(exemples)))
+        self.data = pd.read_csv(exemples[path_queries_index])
+        with open(queries_path, 'rb') as file:
+            self.query = file.readlines()[path_queries_index]
+        return self.query, self.data.copy()
 
 
 
