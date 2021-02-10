9fb328c915ba19980c27579193ee8b672249b97f
KameniAlexNea <Kamenialexnea@gmail.com>
2021-02-10 14:47:47 +0100

    update EngineModel : query path and model. download data for nltk. Adding categories columns

--- google_ensp/google_ensp/urls.py
+++ google_ensp/google_ensp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('search_app/', include('search_app.urls')),
+    path('', include('search_app.urls')),
     path('admin/', admin.site.urls),
 ]
--- google_ensp/search_app/models.py
+++ google_ensp/search_app/models.py
@@ -12,7 +12,7 @@ import pickle as pkl
 
 class EngineModel():
     def __init__(self, model_folder="./search_app/ml_models/sauvegarde/"):
-        self.headers = { 
+        self.headers = {
             "apikey": "60e07650-2df1-11eb-96e2-bf23fa38d811"
         }
         self.params = None
@@ -24,19 +24,20 @@ class EngineModel():
         self.lr = None
         self.xgb = None
         self.vectoriser = None
+        self.categories = ["REMEMBERING", "UNDERSTANDING", "APPLYING", "ANALYZING", "EVALUATING", "CREATING"]
         self._read(model_folder)
-    
+
     def _read(self, model_folder="./search_app/search_app/search_app/ml_models/sauvegarde/"):
         """
             Charger les modèles de machine de learning pour la classification
         """
         with open(model_folder+"linear_reg.pkl", "rb") as file:
-            self.lr = pkl.load(file) # Model Logistique
+            self.lr = pkl.load(file)  # Model Logistique
         with open(model_folder+"xgboost.pkl", "rb") as file:
-            self.xgb = pkl.load(file) # Model XGBoost
+            self.xgb = pkl.load(file)  # Model XGBoost
         with open(model_folder+"tfid2.pkl", "rb") as file:
-            self.vectoriser = pkl.load(file) # Model Vectoriser
-    
+            self.vectoriser = pkl.load(file)  # Model Vectoriser
+
     def search(self, query, count=70, local=True):
         if local:
             qr, dt = self._fetch_data_offline()
@@ -44,10 +45,9 @@ class EngineModel():
             qr, dt = self._fetch_data(query, count)
         qr_cl, dt_cl = self._clean_text(query, dt)
         qr_pb, dt_pb = self._classify_base_on_bloom(qr_cl, dt_cl)
-        return (qr, dt), (qr_pb, dt_pb), self.lr.classes_
+        return (qr, dt), (qr_pb, dt_pb), self.categories
 
-
-    def _clean_text(self, question:str, answers:pd.DataFrame, column="description"):
+    def _clean_text(self, question: str, answers: pd.DataFrame, column="description"):
         """
             Nettoyer les données obtenues de la requête et de l'API de recherche
             Params:
@@ -63,23 +63,23 @@ class EngineModel():
         self.data_clean = m.clean_dataframe(reponses)
         self.query = m.clean_dataframe(query)
         return self.query, self.data_clean.copy()
-    
+
     def _classify_base_on_bloom(self, query_clean, data_clean, column="text_clean"):
         """
             Classifier la requete ainsi que le jeu de données d'entrainement
         """
         query_vect = self.vectoriser.transform(query_clean[column].values)
-        query_prob = (self.lr.predict_proba(query_vect)+self.xgb.predict_proba(query_vect))/2
-        query_prob = pd.DataFrame(query_prob, columns=self.lr.classes_)
+        query_prob = (self.lr.predict_proba(query_vect) +
+                      self.xgb.predict_proba(query_vect))/2
+        query_prob = pd.DataFrame(query_prob, columns=self.categories)
 
         data_vect = self.vectoriser.transform(data_clean[column].values)
-        data_prob = (self.lr.predict_proba(data_vect) + self.xgb.predict_proba(data_vect))/2
-        data_prob = pd.DataFrame(np.array(data_prob.reshape(-1, 6)), columns=self.lr.classes_)
-        print('Shape', data_prob.shape)
+        data_prob = (self.lr.predict_proba(data_vect) +
+                     self.xgb.predict_proba(data_vect))/2
+        data_prob = pd.DataFrame(
+            np.array(data_prob.reshape(-1, 6)), columns=self.categories)
         return query_prob, data_prob
-    
 
-    
     def _fetch_exemples(self, path="./search_app/ml_models/exemple/queries.txt", save_to="./search_app/ml_models/exemple/", count=70):
         """
             Fouiller les résultats pertinents des requêtes contenues dans path
@@ -89,7 +89,8 @@ class EngineModel():
             questions = file.readlines()
         for i, question in enumerate(questions):
             _, data = self._fetch_data(question)
-            data.to_csv(save_to+"query{}.csv".format(i), index=False, header=True)
+            data.to_csv(save_to+"query{}.csv".format(i),
+                        index=False, header=True)
 
     def _fetch_data(self, query=None, count=70):
         """
@@ -108,33 +109,38 @@ class EngineModel():
             ("num", str(count)),
         )
         self.query = query
-        self.response = requests.get(self.base_url+"search", headers=self.headers, params=self.params)
+        self.response = requests.get(
+            self.base_url+"search", headers=self.headers, params=self.params)
         self.data = pd.DataFrame(self.response.json()["organic"])
         return query, self.data[self.columns].copy()
 
-    def _fetch_data_offline(self, path='./search_app/ml_models/exemple/query*.csv', queries_path='./search_app/ml_models/exemple/queries.txt'):
-        """
-            Appliquer les différents modèles sur des données en local
-            Params:
-                path: chemin contenant les différents fichier .csv
-                le choix d'une des requêtes du fichier se fait aléatoirement
-            Returns:
-                DataFrame de resultat ainsi que la requête transmise
-                question, answers
-                    - question: str
-                    - answers: pd.DataFrame: ["url", "title", "description", "destination"]
-        """
-        np.random.seed()
-        exemples = glob.glob(path)
-        if len(exemples) == 0:
-            self._fetch_exemples()
+    # google_ensp\search_app\ml_models\exemple\exemple0.csv
+    def _fetch_data_offline(self, path='search_app/ml_models/exemple/query*.csv', queries_path='search_app/ml_models/exemple/queries.txt'):
+            """
+                Appliquer les différents modèles sur des données en local
+                Params:
+                    path: chemin contenant les différents fichier .csv
+                    le choix d'une des requêtes du fichier se fait aléatoirement
+                Returns:
+                    DataFrame de resultat ainsi que la requête transmise
+                    question, answers
+                        - question: str
+                        - answers: pd.DataFrame: ["url", "title", "description", "destination"]
+            """
+            np.random.seed()
             exemples = glob.glob(path)
-            # raise Exception("No queries examples found in {}".format(path))
-        path_queries_index = np.random.choice(range(len(exemples)))
-        self.data = pd.read_csv(exemples[path_queries_index])
-        with open(queries_path, 'rb') as file:
-            self.query = file.readlines()[path_queries_index]
-        return self.query, self.data.copy()
+            if len(exemples) == 0:
+                print('Empty Example')
+                exit()
+                self._fetch_exemples()
+                exemples = glob.glob(path)
+                # raise Exception("No queries examples found in {}".format(path))
+            path_queries_index = np.random.choice(range(len(exemples)))
+            self.data = pd.read_csv(exemples[path_queries_index])
+            self.data.dropna(inplace=True)
+            with open(queries_path, 'rb') as file:
+                self.query = file.readlines()[path_queries_index]
+            return self.query, self.data.copy()
 
 
 class Result(models.Model):
